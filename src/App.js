2482f6783c05e56b86854349ba203ab64b4ff2f6
lee <logicleeee+github@gmail.com>
2019-05-13 16:31:54 -0700

    substituted fakeServerData for some hard coded values

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,11 +12,42 @@ let defaultStyle = {
   'grid-gap': '2rem'
 };
 
+let fakeServerData = {
+  user: {
+    name: 'Lee',
+  playlists: [
+    {
+      name: 'my favs00',
+      songs: ['song001','song002','song003', 'song004', 'song005']
+    },
+    {
+      name: 'my favs10',
+      songs: ['song101','song102','song103', 'song104', 'song105']
+    },
+    {
+      name: 'my favs20',
+      songs: ['song201','song202','song203', 'song204', 'song205']
+    },
+    {
+      name: 'my favs30',
+      songs: ['song301','song302','song303', 'song304', 'song305']
+    },
+    {
+      name: 'my favs40',
+      songs: ['song401','song402','song403', 'song404', 'song405']
+    }
+  ]
+  }
+}
+
 class Aggregate extends React.Component {
   render () {
     return (
         <div style={defaultStyle}>
-        <h2>Number Text</h2>
+        {
+          this.props.playlists &&
+            <h2> {this.props.playlists.length} Playlists</h2>
+        }
         </div>
     );
   }
@@ -47,17 +78,34 @@ class Playlist extends React.Component {
     );
   }
 }
-export default function App() {
-  const myName = 'Lee';
-  return (
-    <div className="App">
+
+//export default function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {serverData: {}};
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+    this.setState({serverData: fakeServerData});
+    }, 2000);
+  }
+
+  render () {
+    return (
+        <div className="App">
         <header className="App-header">
-            <h1 style={{...defaultStyle, color: textTitleColor}}>Lee's Playlists
+        {this.state.serverData.user ?
+         <div>
+        <h1 style={{...defaultStyle, color: textTitleColor}}>
+         {this.state.serverData.user.name}'s Playlists
             </h1>
             <div style={{...defaultStyle,
                         "grid-template-columns": 'repeat(2, 1fr)',
                         }}>
-                <Aggregate/>
+                <Aggregate playlists={this.state.serverData.user &&
+                                      this.state.serverData.user.playlists}/>
                 <Aggregate/>
             </div>
             <div style={defaultStyle}>
@@ -72,7 +120,12 @@ export default function App() {
                 <Playlist/>
                 <Playlist/>
             </div>
+        </div>: <h1>Loading...</h1> }
         </header>
     </div>
   );
+  }
 }
+
+
+export default App;
